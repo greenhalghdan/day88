112cc169f8364351152e389e3e38c55eef01e28c
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2024-01-01 19:42:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
+db = SQLAlchemy()
+db.init_app(app)
+
+class cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=False, nullable=False)
+    map_url = db.Column(db.String, unique=False, nullable=False)
+    img_url = db.Column(db.String, unique=False, nullable=False)
+    location = db.Column(db.String, unique=False, nullable=False)
+    has_sockets = db.Column(db.BOOLEAN, unique=False, nullable=False)
+    has_toilet = db.Column(db.BOOLEAN, unique=False, nullable=False)
+    has_wifi = db.Column(db.BOOLEAN, unique=False, nullable=False)
+    can_take_calls = db.Column(db.BOOLEAN, unique=False, nullable=False)
+    seats = db.Column(db.VARCHAR, unique=False, nullable=False)
+    coffee_price = db.Column(db.VARCHAR, unique=False, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
